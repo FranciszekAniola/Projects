2e3dfbdd9fe811992b81bbf782ca4161fb0124c2
FranciszekAniola <104651743+FranciszekAniola@users.noreply.github.com>
2022-06-03 17:20:40 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,134 +1,235 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
+#include <sstream>
 
-struct Answers
-{
-private:
-	std::string Content;
-	bool IsRight;
-	int Qnumber;
-
+class ShipPart {
 public:
-	Answers(std::string content, bool isRight, int QNumber) {
-		this->Content = content;
-		this->IsRight = isRight;
-		this->Qnumber = QNumber;
-	}
-
-	void setContent(std::string content) {
-		this->Content = content;
-	}
-
-	std::string getContent() {
-		return Content;
-	}
-
-	void setIsRight(bool isRight) {
-		this->IsRight = isRight;
-	}
-
-	bool getIsRight() {
-		return IsRight;
-	}
-
-	int getQnumber() {
-		return Qnumber;
-	}
+    int y;
+    int x;
+    bool hit = false;
 };
 
-struct Question
+class Ship
 {
-private:
-	std::string QContent;
-	std::vector<Answers> vec;
-	int Qnumber;
 public:
-	Question(std::string content) {
-		this->QContent = content;
-	}
+    std::vector<ShipPart> ShipPart;
+    int size;
+    std::string typeOfShip;
 
-	void setContent(std::string content) {
-		this->QContent = content;
-	}
+    Ship(int p_size, std::string p_typeOfShip) : size(p_size), typeOfShip(p_typeOfShip) {
 
-	std::string getContentQ() {
-		return QContent;
-	}
-
-	std::vector<Answers> getVec() {
-		return vec;
-	}
-
-	void putAnswerToVec(Answers answer) {
-		vec.push_back(answer);
-
-	}
-
-	void viewQ() {
-		std::cout << QContent << "\n";
-	}
-
-	void viewVec() {
-		int plus = 0;
-
-		for (Answers x : vec) {
-			plus++;
-			std::cout << (vec.size() + plus) - vec.size() << ". " << x.getContent() << "\n";
-		}
-	}
-
+    }
 };
 
-void isCorrect(Question q) {
-	std::cout << "Chose between 1-3" << "\n";
-	int guess;
-	std::cin >> guess;
-	for (Answers x : q.getVec()) {
-		if (guess == x.getQnumber()) {
-			if (x.getIsRight()) {
-				std::cout << "Correct!" << "\n";
-				break;
-			}
-			else {
-				std::cout << "Incorrect! :(" << "\n";
-			}
-		}
-	}
-}
+
+class Board {
+public:
+   
+    std::vector<std::vector<char>> vec =
+    {
+        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, //1
+        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, //2
+        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, //3
+        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, //4
+        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, //5
+        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, //6
+        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, //7
+        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, //8
+        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, //9
+        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, //10
+    };
 
 
+    std::vector<char> collumn { 'a','b','c','d','e','f','g','h','i','j' };
+    std::vector<int> row { 0,1,2,3,4,5,6,7,8,9 };
+
+
+
+    void printBoard() {
+        std::cout << std::endl;
+        std::cout << "Players board: " << std::endl;
+        for (int i = 0; i < row.size(); ++i) {
+            std::cout << "  " << collumn[i] << " ";
+        }
+
+        std::cout << std::endl;
+
+        for (int i = 0; i < vec.size(); ++i) {
+            std::cout << row[i];
+            for (int j = 0; j < vec[i].size(); ++j) {
+                std::cout << '|' << vec[i][j] << "| ";
+            }
+
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+};
+
+
+class Player
+{
+public:
+    std::string name;
+    Board board;
+    std::vector<Ship> shipSet;
+
+    Player(std::string p_name, Board &p_board, std::vector<Ship> p_shipSet) : name(p_name), board(p_board), shipSet(p_shipSet) {}
+};
+
+class GamePlay {
+public:
+    
+    Board board_2;
+
+    void settingUpShips(Player& player) {
+        std::cout << player.name << " is setting his ships" << std::endl;
+
+        int queue = 0;
+
+        std::string coord;
+
+        char shipSign = 'x';
+        std::string vertical = "v";
+        std::string horizontal = "h";
+        
+        std::string numExtrcted = "";
+        std::string letterExtracted = "";
+        std::string axis = "";
+
+
+        // 3 bo sa trzy statki
+        while (queue != 3) {
+
+            std::getline(std::cin, coord);
+
+            numExtrcted = "";
+            letterExtracted = "";
+            axis = "";
+
+            std::string partOfShip;
+            partOfShip = coord[0] + coord[1];
+
+            numExtrcted = coord[1];
+            letterExtracted = coord[0];
+            axis = coord[2];
+
+
+            bool isNumber = isdigit(coord[1]);
+            bool isLetter = (coord[0] >= 'a') && (coord[0] <= 'j');
+
+            int xCoord = 0;
+            int yCoord = 0;
+
+            if (coord.size() <= 3 && isNumber && isLetter) {
+
+                yCoord = stoi(numExtrcted);
+
+                for (int i = 0; i < 10; ++i) {
+                    if (letterExtracted[0] == board_2.collumn[i]) {
+                        xCoord = i;
+                        break;
+                    }
+                }
+            }
+
+            //kolejka gracza ustawiajacego statki trwa tyle iteracji ile jest statkow w jego zestawie, dlatego shipSet[queue]
+            for (int i = 0; i < player.shipSet[queue].size; ++i) {
+                if (axis == vertical) {
+                    if (player.board.vec[yCoord + i][xCoord] != shipSign) {
+
+                        player.board.vec[yCoord + i][xCoord] = shipSign;
+                        //przypisywanie kazdej czesci statku wpolzedne
+                        player.shipSet[queue].ShipPart.push_back({yCoord + i , xCoord});
+                        
+                    } else {
+                        wrongInputInformation();
+                    }
+
+                   
+                }
+                else if (axis == horizontal) {
+                    if (player.board.vec[yCoord][xCoord + i] != shipSign) {
+
+                        player.board.vec[yCoord][xCoord + i] = shipSign;
+                        //przypisywanie kazdej czesci statku wpolzedne
+                        player.shipSet[queue].ShipPart.push_back({ yCoord , xCoord +i});
+                    } else {
+                        wrongInputInformation();
+                    }
+                }
+                else {
+                    if (player.board.vec[yCoord + i][xCoord] != shipSign) {
+
+                        player.board.vec[yCoord][xCoord] = shipSign;
+                        //przypisywanie kazdej czesci statku wpolzedne
+                    }
+                    else {
+                        wrongInputInformation();
+                    }
+                }
+            }
+
+            coord = "";
+            partOfShip = "";
+
+            queue++;
+        }
+    }
+
+    void wrongInputInformation() {
+        std::cout << "Wrong input" << std::endl;
+        exit(1);
+    }
+};
 
 
 int main() {
+    auto firstSetOfShips = std::vector<Ship>{};
+    auto secondSetOfShips = std::vector<Ship>{};
 
-	Answers firstQA("21", true,1);
-	Answers firstQA2("2", false,2);
-	Answers firstQA3("5", false,3);
+    Ship twoSailsShip_1(2, "Dwumasztowiec");
+    Ship oneSailsShip_1(1, "jednomasztowiec");
+    Ship threeSailsShip_1(3, "trzymasztowiec");
+    
+    
+    Ship twoSailsShip_2(2, "Dwumasztowiec");
+    Ship oneSailsShip_2(1, "jednomasztowiec");
+    Ship threeSailsShip_2(3, "trzymasztowiec");
 
-	Question firstQ("which number is the biggest?\n");
-	firstQ.putAnswerToVec(firstQA);
-	firstQ.putAnswerToVec(firstQA2);
-	firstQ.putAnswerToVec(firstQA3);	
+    firstSetOfShips.push_back(twoSailsShip_1);
+    firstSetOfShips.push_back(oneSailsShip_1);
+    firstSetOfShips.push_back(threeSailsShip_1);
 
-	Answers secondQA("warsaw", false,1);
-	Answers secondQA2("Berlin", false,2);
-	Answers secondQA3("Budapest", true,3);
+    secondSetOfShips.push_back(twoSailsShip_2);
+    secondSetOfShips.push_back(oneSailsShip_2);
+    secondSetOfShips.push_back(threeSailsShip_2);
+    
+    
+    Board board_1;
+    Board board_2;
+        
+    
+    Player p1("Player one", board_1, firstSetOfShips);
+    Player p2("Player two", board_2, secondSetOfShips);
+    
+    GamePlay game;
 
-	Question secondQ("which is the cappital of Hungary?\n");
-	secondQ.putAnswerToVec(secondQA);
-	secondQ.putAnswerToVec(secondQA2);
-	secondQ.putAnswerToVec(secondQA3);
+    game.settingUpShips(p1);
+    p1.board.printBoard();
 
-	firstQ.viewQ();
-	firstQ.viewVec();
-
-	isCorrect(firstQ);
-
-	secondQ.viewQ();
-	secondQ.viewVec();
-
-	isCorrect(secondQ);
+   
+       std::cout << p1.shipSet[2].ShipPart[0].x;
+    
+    std::cout << std::endl;
+    
+      std::cout << p1.shipSet[2].ShipPart[0].y;
+    
+   // game.settingUpShips(p2);
+   // p2.board.printBoard();
 
 
+    
+ 
 }
